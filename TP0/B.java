2d69c61cdde6762686845dd4395119ab30610b1a
Hany Abboud <hany.abboud@isae.edu.lb>
2019-02-28 07:50:52 -1000

    Tester ex1 cours

---
+++ TP0/B.java
@@ -0,0 +1,10 @@
+public class B extends A
+{
+    void m(A a){
+        System.out.println("m de B");
+    }
+    
+    void n(B b){
+        System.out.println("n de B");
+    }
+}
---
+++ TP0/TP0.java
@@ -0,0 +1,10 @@
+public class TP0
+{
+    public static void main(String args[]){
+        A a = new B();
+        B b = new B();
+        
+        a.m(b);
+        a.n(b);
+    }
+}
